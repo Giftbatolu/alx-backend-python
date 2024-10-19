11a75a1a7bac90de20a904de53d6cd33374de10c
Giftbatolu <sekinatoyelami@gmail.com>
2024-10-19 01:35:51 +0000

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,13 +4,17 @@
 
 import asyncio
 import random
-from typing import List
+from typing import AsyncGenerator
 
 
-async def async_generator(None) -> List[float]:
+async def async_generator() -> AsyncGenerator[float, None]:
+
     """
         A coroutine function that takes no argument and
         return a list of float.
+
+        Yield:
+            A list of random number betwwen 0 to 10
     """
     for _ in range(10):
         rand_num = random.uniform(0, 10)
