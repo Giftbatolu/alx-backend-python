fd0450522610d1a30d494097682bdb66274b7702
giftbatolu <sekinatoyelami@gmail.com>
2025-11-03 17:30:19 +0100

    Model: Define the model for the app

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,40 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import django.utils.timezone as timezone
 
-# Create your models here.
+class User(AbstractUser):
+    ROLES = (
+        ('admin', 'Admin'),
+        ('user', 'User'),
+        ('guest', 'Guest'),
+    )
+
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True)
+    email = models.EmailField(unique=True, null=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    role = models.CharField(max_length=10, choices=ROLES, default='user')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f"({self.get_full_name()} {self.role})"
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True)
+    participants_id = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Conversation {self.conversation_id}"
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True)
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Message {self.message_id} from {self.sender.get_full_name()}"
